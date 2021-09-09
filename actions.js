520e9e46ce8e29dfeb06dc9c89c319102b1841ba
11parul <85674158+11parul@users.noreply.github.com>
2021-09-09 16:12:21 +0530

    Update actions.js

--- actions.js
+++ actions.js
@@ -8,7 +8,7 @@ img1.src = src;
 };
 
 let imagePath = (frameNumber,animation) => {
-    return "pics/" +website+"/"+ frameNumber + ".png";
+    return "pics/" + frameNumber + ".png";
 };
 
 let frames = {
